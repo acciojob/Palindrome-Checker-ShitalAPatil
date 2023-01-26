48b33dd615646300c16c7f6fd9533086d9d1f0eb
ShitalAPatil <shitalpatilb29@gmail.com>
2023-01-26 18:20:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // complete the given function
 
-function palindrome(str){
+// function palindrome(str){
     
-        var reverseStr= str.trim().split("").reverse().join("").toLowerCase()
-        if(reverseStr===str.trim()){
-            return true
-        } else {
-            return false
-        }
+//         var reverseStr= str.trim().split("").reverse().join("").toLowerCase()
+//         if(reverseStr===str.trim()){
+//             return true
+//         } else {
+//             return false
+//         }
 
+// }
+// module.exports = palindrome
+
+// complete the given function
+
+function palindrome(str){
+    const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
+    // 2) Strip out non-alphanumeric characters
+    
+// 3) return string === reversedString
+return alphanumericOnly.join('') === alphanumericOnly.reverse().join('');
+    
 }
+console.log(palindrome("racecar"));
+
 module.exports = palindrome
